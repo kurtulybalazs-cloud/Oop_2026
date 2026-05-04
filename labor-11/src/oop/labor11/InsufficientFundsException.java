112db7345f0c393436654145605ca85f18143ba8
kurtulybalazs-cloud <kurtuly.balazs@student.ms.sapientia.ro>
2026-05-04 15:36:18 +0300

    feladatok 2 es 3

---
+++ labor-11/src/oop/labor11/InsufficientFundsException.java
@@ -0,0 +1,7 @@
+package oop.labor11;
+
+public class InsufficientFundsException extends RuntimeException {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
--- labor-11/src/oop/labor11/Main.java
+++ labor-11/src/oop/labor11/Main.java
@@ -1,7 +1,40 @@
 package oop.labor11;
 
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Lab_11");
+
+
+
+
+
+
+
+
+    public static int safeDivide(int a, int b){
+        if (b == 0) throw new RuntimeException("DIVISION_BY_ZERO");
+        return a/b;
+
+    }
+
+    public static void demonstrateFinally(){
+        try {
+            System.out.println("try ban vagzunk");
+            throw new RuntimeException("stop)");
+        }catch(Exception e){
+            System.out.println("catch ben vagyunk");
+            throw e;
+        }
+        finally{
+            System.out.println("Finally ben vagyunk");
+        }
+    }
+
+    public static void main (String[]args){
+
+        demonstrateFinally();
+        System.out.println("a/b = " + safeDivide(1,0));
     }
 }
