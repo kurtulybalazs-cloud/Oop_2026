b7d000de0a3e4df55f5247575db1d2f9b16eedf3
kurtulybalazs-cloud <kurtuly.balazs@student.ms.sapientia.ro>
2026-05-11 15:21:35 +0300

    lab9_2

---
+++ labor-09/src/oop/labor09/lab9_2/ArrayListDictionary.java
@@ -0,0 +1,17 @@
+package oop.labor09.lab9_2;
+
+import java.util.ArrayList;
+
+public class ArrayListDictionary {
+
+    private ArrayList<String> words  ;
+    private static ArrayListDictionary instance;
+
+    private void ArrayListDictionary(){
+
+    }
+
+    public static IDictionary newInstance(){
+
+    }
+}
---
+++ labor-09/src/oop/labor09/lab9_2/IDictionary.java
@@ -0,0 +1,17 @@
+package oop.labor09.lab9_2;
+
+public interface IDictionary {
+    public static final String DICTIONARY_FILE = "";
+
+    public boolean add (String ){
+
+    }
+
+    public boolean find(String){
+
+    }
+
+    public int size(){
+
+    }
+}
